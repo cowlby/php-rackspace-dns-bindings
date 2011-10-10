463132c81aa5f4eaece0f121f77f7221c78303f6
Jose Prado <pradador@me.com>
2011-10-10 11:47:59 -0500

    Implemented createException factory method.

--- src/Prado/Rackspace/DNS/Exception/CloudDnsFault.php
+++ src/Prado/Rackspace/DNS/Exception/CloudDnsFault.php
@@ -7,8 +7,29 @@ use RuntimeException;
 
 class CloudDnsFault extends RuntimeException implements CloudDnsException
 {
+    public static $faults = array(
+        400 => 'Prado\Rackspace\DNS\Exception\BadRequestFault',
+        401 => 'Prado\Rackspace\DNS\Exception\UnauthorizedFault',
+        404 => 'Prado\Rackspace\DNS\Exception\ItemNotFoundFault',
+        409 => 'Prado\Rackspace\DNS\Exception\OverLimitFault',
+        500 => 'Prado\Rackspace\DNS\Exception\CloudDnsFault'
+    );
+    
     public function __construct($statusCode, $response = NULL, Exception $previous = NULL)
     {
         parent::__construct($response, $statusCode, $previous);
+    }
+    
+    public static function createException($statusCode, $response = NULL, Exception $previous = NULL)
+    {
+        $class = NULL;
+        
+        if (isset(self::$faults[$statusCode])) {
+            $class = self::$faults[$statusCode];
+        } else {
+            $class = 'Prado\Rackspace\DNS\Exception\CloudDnsFault';
+        }
+        
+        return new $class($statusCode, $response, $previous);
     }
 }
