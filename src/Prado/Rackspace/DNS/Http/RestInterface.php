6246e0b741255eda894ba8e05cd1e6aea9f2a6d1
Jose Prado <pradador@me.com>
2011-10-10 12:30:48 -0500

    Added PHPDocs to the RestInterface interface.

--- src/Prado/Rackspace/DNS/Http/RestInterface.php
+++ src/Prado/Rackspace/DNS/Http/RestInterface.php
@@ -4,11 +4,37 @@ namespace Prado\Rackspace\DNS\Http;
 
 interface RestInterface
 {
+    /**
+     * GETs the specified path from the Cloud DNS Service.
+     * 
+     * @param string $path The path to the resource
+     * @return string The server response
+     */
     public function get($path);
     
+    /**
+     * POSTs the specified data to the path on the Cloud DNS Service.
+     * 
+     * @param string $path The path to the resource
+     * @param array  $data An array of json-ready data to send
+     * @return string The server response
+     */
     public function post($path, array $data);
     
+    /**
+     * PUTs the specified data to the path on the Cloud DNS Service.
+     * 
+     * @param string $path The path to the resource
+     * @param array  $data An array of json-ready data to send
+     * @return string The server response
+     */
     public function put($path, array $data);
     
+    /**
+     * DELETEs the specified resource from the Cloud DNS Service.
+     * 
+     * @param string $path The path to the resource
+     * @return string The server response
+     */
     public function delete($path);
 }
